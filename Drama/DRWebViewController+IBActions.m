91fe4070f3013cf4c2cac83d25d6c06ea4c8b96e
Shinren Pan <shinren.pan@gmail.com>
2015-11-23 16:28:50 +0800

    新增清除網頁快取. 新增清除觀看紀錄.

--- Drama/DRWebViewController+IBActions.m
+++ Drama/DRWebViewController+IBActions.m
@@ -23,8 +23,7 @@
 #pragma mark - BackButton long press
 - (IBAction)backButtonLongPressed:(UILongPressGestureRecognizer *)sender
 {
-    if(sender.state == UIGestureRecognizerStateBegan)
-    {
+    if(sender.state == UIGestureRecognizerStateBegan) {
         NSArray *history = self.webView.backForwardList.backList;
         
         [self performSegueWithIdentifier:@"toDRHistoryViewController" sender:history];
@@ -40,16 +39,68 @@
 #pragma mark - ForwardButton long press
 - (IBAction)forwardButtonLongPressed:(UILongPressGestureRecognizer *)sender
 {
-    if(sender.state == UIGestureRecognizerStateBegan)
-    {
+    if(sender.state == UIGestureRecognizerStateBegan) {
         NSArray *history = self.webView.backForwardList.forwardList;
         
         [self performSegueWithIdentifier:@"toDRHistoryViewController" sender:history];
     }
 }
 
-#pragma mark - 加入書籤 clicked
-- (IBAction)addItemClicked:(id)sender
+#pragma mark - Action clicked
+- (IBAction)actionItemClicked:(id)sender
+{
+    UIAlertController *alert =
+    [UIAlertController alertControllerWithTitle:nil
+                                        message:nil
+                                 preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    // iPad 要用 POPOver 才能顯示 ActionSheet, 不然會 Crash.
+    if(UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+        UIPopoverPresentationController *pop = [alert popoverPresentationController];
+        pop.sourceView = self.view;
+        
+        // 44 為 macigc number, 為最右邊的 UIBarButtonItem.
+        // 千萬不要使用 pop.barButtonItem = sender,
+        // 因為使用了當 Popover 出現時, 可以點擊其他 item 會造成 bug.
+        pop.sourceRect = ({
+            CGRect temp = CGRectZero;
+            temp.origin.x = CGRectGetMaxX(self.navigationController.navigationBar.frame) - 44;
+            temp.origin.y = CGRectGetMaxY(self.navigationController.navigationBar.frame);
+            temp;
+        });
+    }
+    
+    UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"取消"
+                                                     style:UIAlertActionStyleCancel
+                                                   handler:nil];
+    
+    UIAlertAction *addToBookmarks = [UIAlertAction actionWithTitle:@"加入書籤"
+                                                             style:UIAlertActionStyleDefault
+                                                           handler:^(UIAlertAction * _Nonnull action) {
+        [self __addToBookmarks];
+    }];
+    
+    UIAlertAction *cleanWebCache = [UIAlertAction actionWithTitle:@"清除網頁快取"
+                                                         style:UIAlertActionStyleDefault
+                                                       handler:^(UIAlertAction * _Nonnull action) {
+        [self __cleanWebCache];
+    }];
+    
+    UIAlertAction *cleanWatchHistory = [UIAlertAction actionWithTitle:@"清除觀看紀錄"
+                                                                style:UIAlertActionStyleDefault
+                                                              handler:^(UIAlertAction * _Nonnull action) {
+        [self __cleanWatchHistory];
+    }];
+    
+    [alert addAction:cancel];
+    [alert addAction:addToBookmarks];
+    [alert addAction:cleanWebCache];
+    [alert addAction:cleanWatchHistory];
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+#pragma mark - Private
+- (void)__addToBookmarks
 {
     if(!self.webView.title.length || !self.webView.URL) {
         return;
@@ -84,6 +135,37 @@
             [alert dismissViewControllerAnimated:YES completion:nil];
         });
     }];
+}
+
+- (void)__cleanWebCache
+{
+    NSSet *websiteDataTypes
+    = [NSSet setWithArray:@[WKWebsiteDataTypeDiskCache,
+                            WKWebsiteDataTypeOfflineWebApplicationCache,
+                            WKWebsiteDataTypeMemoryCache,
+                            WKWebsiteDataTypeLocalStorage,
+                            WKWebsiteDataTypeCookies,
+                            WKWebsiteDataTypeSessionStorage,
+                            WKWebsiteDataTypeIndexedDBDatabases,
+                            WKWebsiteDataTypeWebSQLDatabases]];
+    
+    NSDate *dateFrom = [NSDate dateWithTimeIntervalSince1970:0];
+
+    [[WKWebsiteDataStore defaultDataStore]removeDataOfTypes:websiteDataTypes
+                                              modifiedSince:dateFrom
+                                          completionHandler:^{
+        
+    }];
+}
+
+- (void)__cleanWatchHistory
+{
+    [[NSUserDefaults standardUserDefaults]removeObjectForKey:@"watchHistory"];
+    [[NSUserDefaults standardUserDefaults]synchronize];
+    
+    [[NSNotificationCenter defaultCenter]
+     postNotificationName:WatchHistoryDeleteNotification
+     object:nil];
 }
 
 @end
